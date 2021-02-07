196fa68b8fa209eaa2862ae274b14f7b9cfc7725
BruLekichLouro <brunalekich@gmail.com>
2021-02-07 16:43:27 -0300

    Relembrando laços de repetições

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-/*var nome = "Bruna Louro";
-var idade = 37;
-alert(nome + " tem " + idade + " anos");*/
+var count = 0;
+for(count = 0; count <=5; count++){
+    alert(count);
+};
 
-/*var frase = "O Brasil tem o melhor time";
+
+/*var count =0;
+while(count <= 5){
+    console.log(count);
+    count ++;
+}
+
+var idade = prompt("Qual sua idade?");
+if(idade>18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+    }
+
+var nome = "Bruna Louro";
+var idade = 37;
+alert(nome + " tem " + idade + " anos");
+
+var frase = "O Brasil tem o melhor time";
 alert(frase.replace("Brasil", "Japão"));
 
 var lista = ["maçã", "pera", "laranja"];
@@ -15,7 +34,7 @@ console.log(lista);
 console.log(lista.length);
 console.log(lista.reverse());
 console.log(lista.toString());
-console.log(lista.join(" | "));*/
+console.log(lista.join(" | "));
 
 var frutas = [{nome: "Maçã", cor: "vermelha"}, {nome: "maçã", cor:"roxa"}]
-console.log(frutas[1].nome);
+console.log(frutas[1].nome);*/
