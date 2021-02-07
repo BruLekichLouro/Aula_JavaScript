1ba49f9e09e9b35c490a962f3430cf82aa8af1a0
BruLekichLouro <brunalekich@gmail.com>
2021-02-07 13:57:45 -0300

    Relembrando conceitos: alert, console.log(), replace, to LowerCase(), toUpperCase(), push e pop.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*var nome = "Bruna Louro";
+var idade = 37;
+alert(nome + " tem " + idade + " anos");*/
+
+/*var frase = "O Brasil tem o melhor time";
+alert(frase.replace("Brasil", "Japão"));*/
+
+var lista = ["maçã", "pera", "laranja"];
+console.log(lista);
+console.log(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
