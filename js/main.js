2c41ed779804bd2152e2c16766669fb631d9f293
BruLekichLouro <brunalekich@gmail.com>
2021-02-07 16:21:28 -0300

    Relembrando listas e dicionários

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var idade = 37;
 alert(nome + " tem " + idade + " anos");*/
 
 /*var frase = "O Brasil tem o melhor time";
-alert(frase.replace("Brasil", "Japão"));*/
+alert(frase.replace("Brasil", "Japão"));
 
 var lista = ["maçã", "pera", "laranja"];
 console.log(lista);
@@ -12,3 +12,10 @@ lista.push("uva");
 console.log(lista);
 lista.pop();
 console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" | "));*/
+
+var frutas = [{nome: "Maçã", cor: "vermelha"}, {nome: "maçã", cor:"roxa"}]
+console.log(frutas[1].nome);
